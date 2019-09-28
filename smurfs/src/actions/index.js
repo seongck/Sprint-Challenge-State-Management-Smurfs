67722437e7e883b062d20e04e2d43cc6ed44a9d4
seongck <seongk@pm.me>
2019-09-27 19:51:48 -0700

    Add POST action creator and update const names

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,21 +1,37 @@
+import axios from "axios";
+
 export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
 export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
 export const FETCH_SMURFS_FAIL = "FETCH_SMURFS_FAIL";
 
-export const POST_SMURFS_START = "POST_SMURFS_START";
-export const POST_SMURFS_FAIL = "POST_SMURFS_FAIL";
-export const POST_SMURFS_SUCCESS = "POST_SMURFS_SUCCESS";
+export const POST_SMURF_START = "POST_SMURFS_START";
+export const POST_SMURF_FAIL = "POST_SMURFS_FAIL";
+export const POST_SMURF_SUCCESS = "POST_SMURFS_SUCCESS";
 
 export const fetchSmurfs = () => {
   return function(dispatch) {
     dispatch({ type: FETCH_SMURFS_START });
-    fetch('http://localhost:3333/smurfs')
-      .then( res => res.json())
-      .then( res => {
-        console.log('reducers/index.js: fetch res: ', res);
-        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res });
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        console.log("reducers/index.js: axios.get res.data: ", res.data);
+        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
       })
-      .catch( err => dispatch({ type: FETCH_SMURFS_FAIL, payload: err })
+      .catch(err =>
+        dispatch({ type: FETCH_SMURFS_FAIL, payload: err.message })
       );
-  }
-}
+  };
+};
+
+export const postSmurf = smurfData => {
+  return function(dispatch, smurfData) {
+    dispatch({ type: POST_SMURF_START });
+    axios
+      .post("http://localhost:3333/smurfs")
+      .then(res => {
+        console.log("reducers/index.js: post res: ", res);
+        dispatch({ type: POST_SMURF_SUCCESS, payload: res });
+      })
+      .catch(err => dispatch({ type: POST_SMURF_FAIL, payload: err }));
+  };
+};
