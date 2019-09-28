9befc98d9a95ca3856d4be4539d52e2d909d9f5c
seongck <seongk@pm.me>
2019-09-27 21:08:45 -0700

    Import and render SmurfForm.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import SmurfList from "./SmurfList";
+import SmurfForm from "./SmurfForm";
 
 function App() {
   return (
     <div className="App">
       <SmurfList />
+      <SmurfForm />
     </div>
   );
 }
